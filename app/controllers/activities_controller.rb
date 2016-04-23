2889c2d7aec11678e7f79fd6f0f8470aa5901bb9
Nipun Monga <monga_nipun@bah.com>
2016-04-23 13:24:51 -0700

    Adding endpoint to retrieve all activity names

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+class ActivitiesController < ApplicationController
+
+  def index
+    uri = URI("https://ridb.recreation.gov/api/v1/activities.json?apikey=#{ENV['RIDB_API_KEY']}")
+    res = Net::HTTP.get(uri)
+    activity_names = []
+    activities = JSON.parse(res)['RECDATA']
+    activities.each do |activity|
+      activity_names << {:activity_name => activity['ActivityName'].titleize}
+    end
+
+    render json: {activities: activity_names.to_json}
+  end
+
+end
