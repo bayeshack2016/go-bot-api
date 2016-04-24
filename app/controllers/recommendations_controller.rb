6c0d2ea8d7c5d35e4e99279e6ab396cd5f4c71c3
Nipun Monga <monga_nipun@bah.com>
2016-04-23 19:02:30 -0700

    Fixing json response for recommendations

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -39,7 +39,7 @@ class RecommendationsController < ApplicationController
   end
 
   def get_image_url(latitude,longitude)
-    uri = URI("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=62b32c5d41d73672e2953c91a317de3c&lat=#{latitude}&lon=#{longitude}&radius=5&page=100&format=json&nojsoncallback=1&extras=description,license,date_upload,date_taken,owner_name,icon_server,original_format,last_update,geo,tags,machine_tags,o_dims,views,media,path_alias,url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o")
+    uri = URI("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=62b32c5d41d73672e2953c91a317de3c&lat=#{latitude}&lon=#{longitude}&radius=5&page=1&format=json&nojsoncallback=1&extras=description,license,date_upload,date_taken,owner_name,icon_server,original_format,last_update,geo,tags,machine_tags,o_dims,views,media,path_alias,url_sq,url_t,url_s,url_q,url_m,url_n,url_z,url_c,url_l,url_o")
     res = Net::HTTP.get(uri)
     value = JSON.parse(res)
 
